7b7a26cbf15183a67b07cd38c422f5d05eccf835
hoheckell <hoheckell.info@gmail.com>
2017-05-31 19:27:50 -0300

    FIRST

---
+++ app/Http/Controllers/MergePdfController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use iio\libmergepdf\Merger;
+
+class MergePdfController extends Controller
+{
+    //
+
+    public function bycsv(Request $request){
+        try {
+
+            /*
+             * Limpa apasta antes de baixar todos os PDFS
+             */
+            $filesnames = glob(public_path('pdfs').'/*'); // get all file names
+            foreach($filesnames as $nameoffile){ // iterate files
+                if(is_file($nameoffile))
+                    unlink($nameoffile); // delete file
+            }
+/*
+ * Lê as urls dos arquivos no CSV e baixa um por um
+ */
+            $files = array();
+            if ($request->hasFile('csv') && $request->file('csv')->isValid()) {
+                $file = $request->file('csv');
+                if (($handle = fopen($file, "r")) !== FALSE) {
+                    while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+                        $num = count($data);
+                        for ($c = 0; $c < $num; $c++) {
+                            $name = explode("/", $data[$c]);
+                            $output_filename = public_path('pdfs') . "/" . end($name);
+
+                            $host = $data[$c];
+                            $ch = curl_init();
+                            curl_setopt($ch, CURLOPT_URL, $host);
+                            curl_setopt($ch, CURLOPT_VERBOSE, 1);
+                            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                            curl_setopt($ch, CURLOPT_AUTOREFERER, false);
+                            // curl_setopt($ch, CURLOPT_REFERER, "http://www.xcontest.org");
+                            curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+                            curl_setopt($ch, CURLOPT_HEADER, 0);
+                            $result = curl_exec($ch);
+                            curl_close($ch);
+                            array_push($files,$output_filename);
+                            // the following lines write the contents to a file in the same directory (provided permissions etc)
+                            $fp = fopen($output_filename, 'w');
+                            fwrite($fp, $result);
+                            fclose($fp);
+                        }
+                    }
+                    fclose($handle);
+                }
+            }
+            /*
+             * Junta todos os PDFs baixados em um só arquivo e exibe o link
+             */
+            $m = new Merger();
+            $m->addIterator($files);
+            file_put_contents(public_path('pdfs') . "/" .'merged.pdf', $m->merge());
+            echo '<a href="';
+            echo asset('pdfs/merged.pdf');
+            echo '">Link</a>';
+
+        }catch (\Exception $e){
+            dd($e->getMessage()." ".$e->getFile()." ".$e->getLine());
+        }
+
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{asset('bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
+    <title>MergePdf</title>
+<style>
+    .barout{
+        border: 1px #ccc solid;
+        height: 30px;
+        width: auto;
+    }
+    .barinner{
+        position: relative;
+        border: none;
+        width: 0%;
+        background-color: #2ca02c;
+        height: 24px;
+        padding-top: 3px;
+        padding-bottom: 5px;
+        display: none;
+    }
+</style>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12">
+            <p>
+            <h1>MergePdf</h1><br>
+            Junção de vários PDFs<br>
+            </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12">
+            <form action="/uploadcsv" method="post" enctype="multipart/form-data" name="formcsv" id="formcsv">
+                {{csrf_field()}}
+                <div class="form-group">
+                    <input type="file" name="csv" id="csv">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-sm btn-primary">Enviar</button>
+                </div>
+            </form>
+            <div class="form-group">
+                <div class="barout">
+                    <div class="barinner">&nbsp;&nbsp;Carregando...</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="{{asset('bower_components/jquery/dist/jquery.js')}}"></script>
+<script src="{{asset('bower_components/bootstrap/dist/js/bootstrap.js')}}"></script>
+<script>
+    $(document).ready(function(){
+        var w=0;
+        $("#formcsv").submit(function(){
+            $(".barinner").show();
+            setInterval(function(){
+                w += 0.5;
+                if(w >=100){
+                    w=0;
+                }else {
+                    $('.barinner').css('width', w + "%");
+                }
+            },1000);
+        });
+    });
+</script>
+</body>
+</html>
